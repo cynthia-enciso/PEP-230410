0e12f39b5f2aad033727986637821b7d65267795
Aleksandr Kunilovskii <17989652+akunilovskii@users.noreply.github.com>
2023-05-22 22:27:05 -0400

    Update UsingQueues.java
    Added some elements to make this example more illustrative

--- Week 7/W7D1 - Collections Part 1/src/UsingQueues.java
+++ Week 7/W7D1 - Collections Part 1/src/UsingQueues.java
@@ -1,72 +1,65 @@
 // queues
-    // head, tail
-    // FIFO
-    // offer = attempt to add
-    // peek = attempt to look at head
-    // poll = attempt to remove
+// FIFO
+// head, tail
+//  offer == attempt to add
+//  peek == attempt to look at head
+//  poll == attempt to remove
 
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
 class Mail {
-    // state
-    String description;
+  // state
+  String description;
+    
+  // constructors
+  public Mail() {}
+   
+  public Mail(String description) {
+    this.description = description;
+  }
 
-    // constructors
-    public Mail(){}
-
-    public Mail(String description) {
-        this.description = description;
-    }
-
-    // toString
-    public String toString() {
-        return description;
-    }
+  public String toString() {
+    return description;
+  }
 }
 
 public class UsingQueues {
 
-    public static ArrayBlockingQueue<Mail> mailbox = new ArrayBlockingQueue<>(5);
-    public static void main(String[] args) {
-        mailmanDepositsMail(10);
+  public static Queue<Mail> mailbox = new ArrayBlockingQueue<>(5);
 
-        // 5 mail items are in the mailbox
+  public static void main(String[] args) {
+    mailmanDepositsMail(10);
+    // 5 mail items are in the mailbox
+    homeownerGetsMail(7);
+    // mailbox.element();
 
-        homeownerGetsMail(7);
-
-        // mailbox is empty!
-        // mailbox.element();
-        System.out.println(mailbox.peek());
-
-    }
-
-    // method for mailman
+    System.out.println(mailbox.peek());
+  }
+    
+    //method for mailman
     public static void mailmanDepositsMail(int amount) {
-
-        boolean wasDeposited = true;
-
-        while (amount > 0) {
-            wasDeposited = mailbox.offer(new Mail());
-            amount--;
-
-            if (wasDeposited) {
-                System.out.println("The mailman deposited an item.");
-            } else {
-                System.out.println("The mailman attempts to deposit mail, but it's full!");
-            }
+      boolean wasDeposited = true;
+      while (amount > 0) {
+        wasDeposited = mailbox.offer(new Mail(String.valueOf(amount)));
+        if (wasDeposited) {
+          System.out.println("The mailman deposited a mail number: " + amount);
+        }else{
+          System.out.println("The mailman attempts to deposit mail number: " + amount + ", but mailbox's full!");
         }
-    }
+        amount--;
+      
+      }}
 
     public static void homeownerGetsMail(int amount) {
-        while (amount > 0) {
-            amount--;
-            if (mailbox.poll() != null) {
-                System.out.println("The homeowner picked up a mail item.");
-            } else {
-                System.out.println("The attempts to grab some mail, but there isn't any!");
-            }
+      while (amount > 0) {
+        amount--;
+        if (mailbox.peek() != null) {
+          System.out.println("The homeowner picked up a mail item number: " + mailbox.poll());
+        } else {
+          System.out.println("The attempts to grab some mail, but there isn't any!");
         }
-    }
+      }
 
-}
+    }
+  }
